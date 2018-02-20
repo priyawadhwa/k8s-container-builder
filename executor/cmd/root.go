d6600b141a1243f7a58a9c95df28a6761afe81bb
Priya Wadhwa <priyawadhwa@google.com>
2018-02-20 11:48:44 -0800

    Added cobra commands

--- executor/cmd/root.go
+++ executor/cmd/root.go
@@ -17,10 +17,22 @@ limitations under the License.
 package cmd
 
 import (
+	"github.com/GoogleCloudPlatform/k8s-container-builder/pkg/constants"
 	"github.com/spf13/cobra"
 )
 
+var (
+	dockerfile string
+	name       string
+	srcContext string
+	logLevel   string
+)
+
 func init() {
+	RootCmd.PersistentFlags().StringVarP(&dockerfile, "dockerfile", "d", "/dockerfile/Dockerfile", "Path to the dockerfile to be built.")
+	RootCmd.PersistentFlags().StringVarP(&srcContext, "context", "c", "", "Path to the dockerfile build context.")
+	RootCmd.PersistentFlags().StringVarP(&name, "name", "n", "", "Registry the final image should be pushed to (ex: gcr.io/test/example:latest)")
+	RootCmd.PersistentFlags().StringVarP(&logLevel, "verbosity", "v", constants.DefaultLogLevel, "Log level (debug, info, warn, error, fatal, panic")
 }
 
 var RootCmd = &cobra.Command{
--- kbuild/cmd/root.go
+++ kbuild/cmd/root.go
@@ -17,14 +17,56 @@ limitations under the License.
 package cmd
 
 import (
+	"github.com/GoogleCloudPlatform/k8s-container-builder/pkg/constants"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"os"
 )
+
+var (
+	dockerfile string
+	name       string
+	srcContext string
+	logLevel   string
+)
+
+func init() {
+	RootCmd.PersistentFlags().StringVarP(&dockerfile, "dockerfile", "d", "Dockerfile", "Path to the dockerfile to be built.")
+	RootCmd.PersistentFlags().StringVarP(&srcContext, "context", "c", "", "Path to the dockerfile build context.")
+	RootCmd.PersistentFlags().StringVarP(&name, "name", "n", "", "Registry the final image should be pushed to (ex: gcr.io/test/example:latest)")
+	RootCmd.PersistentFlags().StringVarP(&logLevel, "verbosity", "v", constants.DefaultLogLevel, "Log level (debug, info, warn, error, fatal, panic")
+}
 
 var RootCmd = &cobra.Command{
 	Use:   "kbuild",
 	Short: "kbuild is a CLI tool for building container images with full Dockerfile support without the need for Docker",
 	Long: `kbuild is a CLI tool for building container images with full Dockerfile support. It doesn't require Docker,
 			and builds the images in a Kubernetes cluster before pushing the final image to a registry.`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		lvl, err := logrus.ParseLevel(logLevel)
+		if err != nil {
+			return errors.Wrap(err, "parsing log level")
+		}
+		logrus.SetLevel(lvl)
+		if err := checkFlags(); err != nil {
+			return err
+		}
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 	},
+}
+
+func checkFlags() error {
+	if srcContext == "" {
+		return errors.Errorf("Please provide source context with --context or -c flag")
+	}
+	if name == "" {
+		return errors.Errorf("Please provide name of final image with --name or -n flag")
+	}
+	if _, err := os.Stat(dockerfile); err != nil {
+		return errors.Wrap(err, "please provide valid path to Dockerfile with --dockerfile or -d flag")
+	}
+	return nil
 }
